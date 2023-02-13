810aec0b606969502f5220378043fe4d08fe278f
Jayce Lovell <19291090+JayceLovell@users.noreply.github.com>
2023-02-13 18:41:17 -0500

    eh

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    void OnOne()
+    {
+
+    }
+    void OnTwo()
+    {
+
+    }
+    void OnThree()
+    {
+
+    }
+    void OnFour()
+    {
+
+    }
+    void OnFive()
+    {
+
+    }
+    void OnSix()
+    {
+
+    }
+    void OnSeven()
+    {
+
+    }
+    void OnEight()
+    {
+
+    }
+    void OnNine()
+    {
+
+    }
+}
